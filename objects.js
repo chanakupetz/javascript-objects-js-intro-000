a1f3aab0811e85d50239c43eece181bc939d70eb
Chana Kupetz <chanakupetz@gmail.com>
2020-03-04 02:11:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,15 @@
-var playlist = {
+var playlist = {"The Beatles": "Blackbird", Radiohead: "Just", "Kanye West": "Good Morning"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign(playlist, {[artistName]: songTitle});
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
+
+/* var playlist = {
   Pink:"Just Like Fire"
   Slowdive: "Alison"
 };
@@ -10,4 +21,4 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist (playlist, Slowdive) {
   delete playlist.Slowdive;
   return playlist;
-}
+} */
